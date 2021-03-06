d12b18eee6b138851b3a20ba4fe5e7d206382094
prince <princeakpabio90@gmail.com>
2021-03-06 03:13:03 +0100

    added graphql mutations api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useQuery, useMutation, gql } from "@apollo/client";
 
+const BANNERMUTATION = gql`
+  mutation UpdateBannerComponents(
+    $id: ID!
+    $brTitle: String
+    $brSubtitle: String
+  ) {
+    updatebannerComponents(
+      where: { id: $id }
+      data: { brTitle: $brTitle, brSubtitle: $brSubtitle }
+    ) {
+      id
+      brTitle
+      brSubtitle
+    }
+  }
+`;
+
+const bannerQuery = gql`
+  query BannerComponents {
+    bannerComponents {
+      id
+      brTitle
+      brSubtitle
+    }
+  }
+`;
 function App() {
+  const [form, setForm] = useState({
+    brTitle: "",
+    brSubtitle: "",
+  });
+  const { loading, error, data } = useQuery(bannerQuery);
+  const [updatebannerComponents, { errorBr }] = useMutation(BANNERMUTATION, {
+    refetchQueries: ["BannerComponents"],
+  });
+  console.log(errorBr);
+  // console.log(data.bannerComponents[0]);
+  const handleInputOnChange = ({ currentTarget: { name, value } }) =>
+    setForm((state) => ({ ...state, [name]: value }));
+
+  // const handleSubmit = (e, item) => {
+
+  // };
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>ERROR: {error}</p>;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.bannerComponents.map((item, i) => (
+        <div key={i}>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              updatebannerComponents({
+                variables: {
+                  id: item.id,
+                  brTitle: form.brTitle,
+                  brSubtitle: form.brSubtitle,
+                },
+              });
+              console.log(form.brSubtitle, form.brTitle);
+              setForm({
+                brTitle: "",
+                brSubtitle: "",
+              });
+            }}
+          >
+            <input
+              type="text"
+              name="brTitle"
+              onChange={handleInputOnChange}
+              value={form.brTitle}
+            />
+            <input
+              type="text"
+              name="brSubtitle"
+              onChange={handleInputOnChange}
+              value={form.brSubtitle}
+            />
+            <input type="submit" value="SUBMIT" />
+          </form>
+
+          <p>{item.brTitle}</p>
+          <p>{item.brSubtitle}</p>
+        </div>
+      ))}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./assets/base.scss";
+import App from "./App";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import reportWebVitals from "./reportWebVitals";
 
+const client = new ApolloClient({
+  uri:
+    "https://api-eu-central-1.graphcms.com/v2/ckikrtqbzzng801xk4ce2fppe/master",
+  cache: new InMemoryCache(),
+});
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
